e45bbb83abacf582f865ce9ec7a0b3d414dce166
Avinash Dewangan <avinash.dew9@gmail.com>
2021-06-07 21:34:21 +0530

    first commit webpack

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # webpack-config
+
+Install some package
+
+npm i -D webpack webpack-cli webpack-dev-server
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const car = {
+    name: 'bmw',
+    year: 2020,
+    price: "$20000"
+}
+
+console.log(car)
